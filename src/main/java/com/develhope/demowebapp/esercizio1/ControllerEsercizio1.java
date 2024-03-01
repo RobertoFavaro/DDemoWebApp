ea2e11276de7bb60453d2b2a3f1014b5185453a4
RobertoFavaro <robyfavaro04@gmail.com>
2024-03-01 14:21:58 +0100

    esercizio Controller con GET completa

--- src/main/java/com/develhope/demowebapp/esercizio1/ControllerEsercizio1.java
+++ src/main/java/com/develhope/demowebapp/esercizio1/ControllerEsercizio1.java
@@ -1,11 +1,9 @@
 package com.develhope.demowebapp.esercizio1;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/es1")
-public class ControllerEsercizio {
+public class ControllerEsercizio1 {
     @GetMapping("/ciaoTempo")
     public String salutaETempo(@RequestParam String nome, @RequestParam String provincia){
         return "Ciao "+ nome+ ", com'è il tempo in "+provincia;
